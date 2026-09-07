e68d6d39039faa3cdcac71fe240be7377a3ea574
Abubakar Muhammad <chzon7@gmail.com>
2026-09-07 16:12:59 +0200

    Initial commit

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
     secure: process.env.NODE_ENV === 'production',
-    httpOnly: true,
+    httpOnly: false,
     sameSite: 'lax'
   }
 }));
